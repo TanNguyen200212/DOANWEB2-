8ead612569c3a8c9bed895edcc9ee5555c4b9ec6
TanNguyen200212 <tanbeer773@gmail.com>
2024-03-30 02:45:00 +0700

    upload source

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+include "view/header.php";
+include "view/home.php";
+include "view/about.php";
+include "view/footer.php";
+
+
+
+?>
---
+++ view/footer.php
@@ -0,0 +1,167 @@
+<footer id="footer" class="my-5">
+    <div class="container py-5 my-5">
+      <div class="row">
+
+        <div class="col-md-3">
+          <div class="footer-menu">
+            <img src="images/logo.png" class= "img-fluid2" alt="logo" width="220px" height="220px">
+            <!-- <p class="blog-paragraph fs-6 mt-3">Subscribe to our newsletter to get updates about our grand offers.</p>
+            <div class="social-links">
+              <ul class="d-flex list-unstyled gap-2">
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:facebook-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:twitter-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:pinterest-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:instagram-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:youtube-fill"></iconify-icon>
+                  </a>
+                </li>
+
+              </ul>
+            </div> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="footer-menu">
+            <h3>Giới thiệu</h3>
+            <ul class="menu-list list-unstyled">
+              <li class="menu-item">
+                <a href="#" class="nav-link">Trang chủ</a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Thú cưng</a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Phụ kiện </a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Dịch vụ</a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Tin tức</a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Liên hệ</a>
+              </li>
+              <li class="menu-item">
+                <a href="#" class="nav-link">Blog chó mèo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="footer-menu">
+            <h3>Hỗ trợ</h5>
+              <ul class="menu-list list-unstyled">
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Hướng dẫn mua hàng</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Chính sách & quy định chung</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Giao nhận và thanh toán</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Đổi trả & bảo hành</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Chính sách khiếu nại</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Chính sách kiểm hàng</a>
+                </li>
+                <li class="menu-item">
+                  <a href="#" class="nav-link">Chính sách bảo mật thông tin</a>
+                </li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div>
+            <h3>Liên hệ</h3>
+            <p class="blog-paragraph fs-6">Điện thoại: 0939 863 696 - 0903 114 822</p>
+            <p class="blog-paragraph fs-6">Email: petshop@gmail.com</p>
+            <p class="blog-paragraph fs-6">Địa chỉ 1: 294 -296 Đồng Đen, Quận Tân Bình, TP Hồ Chí Minh</p>
+            <p class="blog-paragraph fs-6">Địa chỉ 2: 313 Hoàng Diệu, P6, Quận 4, TP Hồ Chí Minh, Việt Nam</p>
+            <div class="search-bar1 border rounded-pill border-dark-subtle px-2">
+              <form class="text-center d-flex align-items-center" action="" method="">
+                <input type="text" class="form-control border-0 bg-transparent" placeholder="Enter your email here" />
+                <iconify-icon class="send-icon" icon="tabler:location-filled"></iconify-icon>
+              </form>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <div id="footer-bottom">
+    <div class="container">
+      <hr class="m-0" width="2000px">
+      <div class="row mt-3">
+        <div class="col-md-6 copyright">
+          <p class="secondary-font">© 2024 Waggy. All rights reserved.</p>
+        </div>
+        <p class="blog-paragraph fs-6 mt-3">Subscribe to our newsletter to get updates about our grand offers.</p>
+            <div class="social-links">
+              <ul class="d-flex list-unstyled gap-2">
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:facebook-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:twitter-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:pinterest-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:instagram-fill"></iconify-icon>
+                  </a>
+                </li>
+                <li class="social">
+                  <a href="#">
+                    <iconify-icon class="social-icon" icon="ri:youtube-fill"></iconify-icon>
+                  </a>
+                </li>
+
+              </ul>
+            </div>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="js/jquery-1.11.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+  <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
